c65a9946ef2361fd03939dc20b5b4bb37cf456a0
egar-learner <75212187+egar-learner@users.noreply.github.com>
2022-01-13 03:12:02 +0530

    day5 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_application/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:flutter_first_application/pages/home_page.dart';
 import 'package:flutter_first_application/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,8 +32,8 @@ class MyApp extends StatelessWidget {
       //initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
       },
     );
   }
